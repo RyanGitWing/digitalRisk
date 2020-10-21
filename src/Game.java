2ef62acf7d62e49602dd5c41898bd119096561a1
Ryan Nguyen <ryannguyen3@cmail.carleton.ca>
2020-10-20 22:02:11 -0400

    Added some commands that still haven't been completed and a four words command.

---
+++ src/Game.java
@@ -0,0 +1,174 @@
+import java.util.*;
+
+public class Game
+{
+    private Parser parser;
+
+    /**
+     * Create the game and initialise its internal map.
+     */
+    public Game()
+    {
+        parser = new Parser();
+    }
+
+    /**
+     *  Main play routine.  Loops until end of play.
+     */
+    public void play()
+    {
+        printWelcome();
+
+        // Enter the main command loop.  Here we repeatedly read commands and
+        // execute them until the game is over.
+
+        boolean finished = false;
+        while (! finished) {
+            Command command = parser.getCommand();
+            finished = processCommand(command);
+        }
+        System.out.println("Thank you for playing.  Good bye.");
+    }
+
+    /**
+     * Print out the opening message for the player.
+     */
+    private void printWelcome()
+    {
+        System.out.println();
+        System.out.println("Welcome to Risk!");
+        System.out.println("Risk is a turn-based world domination game.");
+        System.out.println("Type 'help' if you need help.");
+        System.out.println();
+        printDescription();
+        System.out.println();
+    }
+
+    /**
+     * This is the method would print the status of the game?
+     */
+    private void printDescription()
+    {
+
+    }
+
+    /**
+     * Given a command, process (that is: execute) the command.
+     * @param command The command to be processed.
+     * @return true If the command ends the game, false otherwise.
+     */
+    private boolean processCommand(Command command)
+    {
+        boolean wantToQuit = false;
+
+        if(command.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+
+        String commandWord = command.getCommandWord();
+        if (commandWord.equals("help")) {
+            printHelp();
+        }
+
+        else if (commandWord.equals("quit")) {
+            wantToQuit = quit(command);
+        }
+
+        else if (commandWord.equals("status")) {
+            status(command);
+        }
+
+        else if (commandWord.equals("attack")) {
+            attack(command);
+        }
+
+        else if (commandWord.equals("endturn")) {
+            endTurn(command);
+        }
+
+        return wantToQuit;
+    }
+
+
+    /**
+     * Print out some help information.
+     * Here we print some stupid, cryptic message and a list of the
+     * command words.
+     */
+    private void printHelp()
+    {
+        System.out.println("Your command words are:");
+        System.out.println("quit help status attack endturn");
+    }
+
+
+
+    /**
+     * "Quit" was entered. Check the rest of the command to see
+     * whether we really quit the game.
+     * @return true, if this command quits the game, false otherwise.
+     */
+    private boolean quit(Command command)
+    {
+        if(command.hasSecondWord()) {
+            System.out.println("Quit what?");
+            return false;
+        }
+        else {
+            return true;  // signal that we want to quit
+        }
+    }
+
+    /**
+     * "Status" was entered. Check the rest of the command to see
+     * whether we checking the status of the game or not.
+     * @param command The command to be processed.
+     */
+    private void status(Command command) {
+
+        if(command.hasSecondWord()) {
+            System.out.println("Status what?");
+
+        }
+
+    }
+
+    /**
+     * "Attack" was entered. Check the rest of the command to see
+     * where we attacking with how many troops.
+     * @param command The command to be processed.
+     */
+    private void attack(Command command) {
+
+        if(!command.hasSecondWord()) {
+            System.out.println("Attack what?");
+
+        }
+
+        if(!command.hasThirdWord()) {
+            System.out.println("Attack with how many?");
+
+        }
+
+    }
+
+    /**
+     * "EndTurn" was entered. Check the rest of the command to see
+     * whether we really ended our turn.
+     * @param command The command to be processed.
+     */
+    private Boolean endTurn(Command command) {
+
+        if(command.hasSecondWord()) {
+            System.out.println("End turn what?");
+
+        }
+        return true;
+    }
+
+    public static void main (String[] args){
+        Game game = new Game();
+        game.play();
+    }
+}
