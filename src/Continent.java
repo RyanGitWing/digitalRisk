31014f1565cd9a2d11e1d66f2cc83654d319161c
VyasanJ <72280936+VyasanJ@users.noreply.github.com>
2020-10-23 13:18:49 -0400

    Add files via upload

---
+++ src/Continent.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Continent {
+    private ArrayList<Country> countries;
+    private int continentBonus;
+    private String name;
+
+    public Continent(ArrayList<Country> countries,int continentBonus, String name){
+        this.countries = countries;
+
+    }
+
+}
