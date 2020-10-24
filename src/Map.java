4f52a9c0dc0a5d7dac821a0af979e86bab2dc8d3
vikiru <72267229+vikiru@users.noreply.github.com>
2020-10-23 23:11:15 -0400

    Updated Map Class
    Added default constructor, setCountry and setCountries method

--- src/Map.java
+++ src/Map.java
@@ -1,2 +1,31 @@
-public class Map {
+import java.util.HashMap;
+
+public class Map
+{
+    private HashMap <CountryName, Integer> Map;
+    private CountryName countryName;
+
+    public Map ()
+    {
+        Map = new HashMap<>();
+        this.setCountries();
+    }
+
+    public void setCountry (CountryName country)
+    {
+        this.Map.put(country,0);
+    }
+
+    public void setCountries ()
+    {
+        for (CountryName country : CountryName.values())
+        {
+            this.setCountry(country);
+        }
+    }
+
+    public void randAllocation (int PlayerCount)
+    {
+
+    }
 }
