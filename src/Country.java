5075025d98512b5baa45f60ff627f11e09c31071
VyasanJ <72280936+VyasanJ@users.noreply.github.com>
2020-10-25 15:26:48 -0400

    Update Country.java

--- src/Country.java
+++ src/Country.java
@@ -1,6 +1,11 @@
 import java.util.*;
 
-
+/**
+ * Represents the coutries
+ *
+ * Author: Vyasan. J
+ * Version: 10.25.2020
+ */
 public class Country {
 
     private final CountryName name;
@@ -19,31 +24,68 @@ public class Country {
         armyOccupied = 0;
         adjCountries = new HashMap<>();
     }
-
+    
+    /**
+     * Sets the ruler of the country
+     *
+     *@param ruler - the ruler of the country
+     */
     public void setRuler(Player ruler){
         this.ruler = ruler;
     }
-
+    
+     /**
+     * Sets the army occupied in the country
+     *
+     *@param army - amount of army in the country
+     */
     public void addArmyOccupied(int army){
-        armyOccupied += army;
+        armyOccupied = army;
     }
-
+    
+     /**
+     * Sets the countries adjacent to the current country
+     *
+     *@param country - current country
+     *@param adj - adjacent country
+     */
     public void setAdjCountry(CountryName country,List<CountryName> adj){
         adjCountries.put(country,adj);
     }
-
+    
+     /**
+     * Returns countries name
+     *
+     *@return the name of the country
+     */
     public CountryName getName(){
         return this.name;
     }
-
+    
+    /**
+     * Returns countries ruler
+     *
+     *@return the ruler of the country
+     */
     public Player getRuler(){
         return this.ruler;
     }
-
+  
+     /**
+     * Returns army occupied in country
+     *
+     *@return the amount of army in countrty
+     */
     public int getArmyOccupied(){
         return this.armyOccupied;
     }
-
+        
+     /**
+     * Returns adjacent countries
+     *
+     *@param country - the current country 
+     *@return a list of adjacent countries
+     */
     public String getAdjCountries(CountryName country){
         return adjCountries.get(country);
     }
